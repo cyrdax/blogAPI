3835342dae4dfff96377f0ec858a0453d8b1f3a6
Reid Moran <reid@clapforencore.com>
2024-07-08 19:25:10 -0700

    fixing for all users

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -2,10 +2,4 @@ class UserSerializer < ActiveModel::Serializer
   attributes :id, :name, :email, :picture, :bio, :created_at, :updated_at
 
   has_many :posts
-
-  def posts
-    object.posts.map do |post|
-      PostSerializer.new(post, scope: scope, root: false)
-    end
-  end
 end
